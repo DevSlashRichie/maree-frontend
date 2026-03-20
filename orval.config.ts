94e5eab8c3d6460a7d0514b57106cc3659183f48
DevSlashRichie <ricardo@binarysails.com>
2026-03-20 11:40:35 -0600

    feat: added orval env selecting baseurl

--- orval.config.ts
+++ orval.config.ts
@@ -1,5 +1,21 @@
 import { defineConfig } from "orval";
 
+const env = process.env.ENVIRONMENT;
+
+if (!env) throw new Error("Please define ENVIRONEMT env var");
+
+console.log("You are executing orval from: ", env);
+
+const BASE_URL_INDEXES = {
+  DEVELOPMENT: 0,
+  PRODUCTION: 1,
+};
+
+const choosenIndex = BASE_URL_INDEXES[env];
+
+if (typeof choosenIndex !== "number")
+  throw new Error("Invalid base url index.");
+
 export default defineConfig({
   mareeSwr: {
     input: "http://localhost:8383/docs/openapi.json",
@@ -10,6 +26,7 @@ export default defineConfig({
       biome: true,
       baseUrl: {
         getBaseUrlFromSpecification: true,
+        index: choosenIndex,
       },
     },
   },
