6010475fc3067e8ea28a13317b709f4e85179e39
DevSlashRichie <ricardo@binarysails.com>
2026-03-25 11:50:34 -0600

    chore: biome fix

--- orval.config.ts
+++ orval.config.ts
@@ -1,31 +1,6 @@
 import { defineConfig } from "orval";
 import "dotenv/config";
 
-const _env = process.env.ENVIRONMENT;
-
-if (!env) throw new Error("Please define ENVIRONEMT env var");
-
-env = env.toUpperCase();
-
-console.log("You are executing orval from:", env);
-
-const _BASE_URL_INDEXES = {
-  DEVELOPMENT: 0,
-  PRODUCTION: 1,
-};
-
-const _SOURCE_INPUT_URL = {
-  DEVELOPMENT: "http://localhost:8383/docs/openapi.json",
-  PRODUCTION:
-    "https://maree.kindmeadow-92ce4777.centralus.azurecontainerapps.io/docs/openapi.json",
-};
-
-const _choosenIndex = BASE_URL_INDEXES[env];
-
-console.log("Using index:", choosenIndex);
-if (typeof choosenIndex !== "number")
-  throw new Error("Invalid base url index.");
-
 let env = process.env.ENVIRONMENT;
 
 if (!env) throw new Error("Please define ENVIRONEMT env var");
